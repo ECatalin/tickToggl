a477ab431699c221b1cf88ceda5440295adafd28
Catalin Erimia <catalin.erimia@liquidbarcodes.com>
2024-09-27 22:16:51 +0200

    Getting time entries from current day and pretty print json to console

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
+	"time"
 )
 
 type Configuration struct {
@@ -27,16 +29,30 @@ func readConfig() Configuration {
 
 func main() {
 	conf := readConfig()
+	today := time.Now().Format("2006-01-02")
+	tomorrow := time.Now().AddDate(0, 0, 1).Format("2006-01-02")
+
+	fmt.Println("Today's date:", today, "Tomorrow's date:", tomorrow)
+
+	uri := "https://api.track.toggl.com/api/v9/me/time_entries"
+	req, err := http.NewRequest(http.MethodGet, uri, nil)
 
 	// Create a new request
-	req, err := http.NewRequest("GET", "https://api.track.toggl.com/api/v9/me", nil)
 	if err != nil {
 		fmt.Println("Error creating request:", err)
 		return
 	}
 
+	q := req.URL.Query()
+	q.Add("start_date", today)
+	q.Add("end_date", tomorrow)
+	req.URL.RawQuery = q.Encode()
+	fmt.Println(req.URL.String())
+
+	req.Header.Set("Content-Type", "application/json; charset=utf-8")
+
 	// Set up basic authentication
-	// Replace "your_username" and "your_password" with your actual credentials
+	// TODO: Switch to session cookie
 	req.SetBasicAuth(conf.ApiToken, "api_token")
 
 	// Send the request
@@ -56,9 +72,12 @@ func main() {
 	}
 
 	// Print response to console
-	fmt.Println(string(body))
-}
+	var prettyJSON bytes.Buffer
+	error := json.Indent(&prettyJSON, body, "", "\t")
+	if error != nil {
+		fmt.Println("JSON parse error: ", error)
+		return
+	}
 
-func auth() {
-
+	fmt.Println(prettyJSON.String())
 }
